22ef74a5c7cb6e2c7883d101237b3f321230b556
AlexanderBraatz <alex.braatz.g@gmail.com>
2024-02-08 15:37:44 +0000

    chore:Add type annotations for HTML elements in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,16 @@ import { v4 as uuidV4 } from 'uuid';
 
 console.log(uuidV4());
 console.log('Hello World!');
+
+// this code gets html elements and tells typescript what type of element it is
+const list = document.querySelector<HTMLUListElement>('#list'); // the function .querySelector can use <> to tell typescript what type of element it is
+
+// const form = document.getElementById<HTMLFormElement>('#new-task-form'); <--  .getElementById does not need <> to tell typescript what type of element it is
+const form = document.getElementById('new-task-form') as HTMLFormElement | null; // this is another way to tell typescript what type of element it is
+
+const input = document.querySelector<HTMLInputElement>('#new-task-title');
+
+//testing the selectors
+console.log('list:', list);
+console.log('form:', form);
+console.log('input:', input);
